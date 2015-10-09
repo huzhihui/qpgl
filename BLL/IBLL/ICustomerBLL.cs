7dfeae7981669095694864d6de061fcfaae6784e
huzhihui <742032660@qq.com>
2015-10-09 17:39:45 +0800

    BLL层

--- BLL/IBLL/ICustomerBLL.cs
+++ BLL/IBLL/ICustomerBLL.cs
@@ -3,11 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-namespace BLL
+using Model;
+namespace BLL.IBLL
 {
-    public class CustomerBLL
+    public interface ICustomerBLL : IBaseBLL<Customer>
     {
-
     }
 }
