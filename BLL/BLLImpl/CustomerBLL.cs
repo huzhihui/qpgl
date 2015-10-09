96af95b68572289559700b6af4b38a48ca3f6917
huzhihui <742032660@qq.com>
2015-10-09 17:44:34 +0800

    提交

---
+++ BLL/BLLImpl/CustomerBLL.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BLL.IBLL;
+using Model;
+namespace BLL
+{
+    public class CustomerBLL : ICustomerBLL
+    {
+        public int doDelete(Customer inVo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int doInsert()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int doInsert(Customer inVo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int doUpdate(Customer inVo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataTable getTable(Customer inVo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
