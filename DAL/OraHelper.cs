da129604b55cc41b23a05e01735736d0135a1d96
huzhihui <742032660@qq.com>
2015-10-09 10:19:55 +0800

    dbhelp

---
+++ DAL/OraHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.OracleClient;
+using System.Configuration;
+namespace DAL
+{
+    public class OraHelper
+    {
+        private string connectionString = ConfigurationManager.AppSettings["qcgl"].ToString();
+    }
+}
