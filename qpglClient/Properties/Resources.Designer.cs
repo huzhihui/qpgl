58f8b6a06ddb7555fb98ae8444bda665035f2458
huzhihui <huzhihui_c@qq.com>
2015-10-14 15:40:53 +0800

    汽配管理客户端

--- qpglClient/Properties/Resources.Designer.cs
+++ qpglClient/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace qcgl.Properties
+namespace qpglClient.Properties
 {
 
 
@@ -44,7 +44,7 @@ namespace qcgl.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("qcgl.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("qpglClient.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- qpglClient/Properties/Settings.Designer.cs
+++ qpglClient/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace qcgl.Properties
+namespace qpglClient.Properties
 {
 
 
